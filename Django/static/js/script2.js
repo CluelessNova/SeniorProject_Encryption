6fe57f95f0b5d07b3f7dea99eb103f5e645d396c
CluelessNova <57334413+CluelessNova@users.noreply.github.com>
2022-08-04 22:07:00 -0400

    Patch for CSS and JS

---
+++ Django/static/js/script2.js
@@ -0,0 +1,56 @@
+function updateKeyInput(val) {
+    document.getElementById('keySecureness').value = val; 
+  }
+
+function updateRangeInput(val) {
+  document.getElementById('keySecurenessLevel').value = val;
+}
+
+function buttonClick(){
+var num = $('#keySecureness').val();
+console.log(num);
+$.get( "/hello?num="+num, function( data ) {
+    $('#displayKey').text(data);
+});
+}
+
+function encrypt(){
+var key = $('#inputKey').val();
+var mess = $('#orignalMessageInput').val();
+$.get( "/encryptMess?key=" + key + "&mess=" + mess , function( data ) {
+    $('#encryptedMessage').text(data);
+    });
+}
+
+function decrypt(){
+  var key = $('#decryptKey').val();
+  var mess = $('#encryptedMessageInput').val();
+  $.get( "/decryptMess?key=" + key + "&mess=" + mess , function( data ) {
+      $('#decryptedMessage').text(data);
+      });
+  }
+
+function downloadKey(){
+  window.location.href='data:application/txt,'+ encodeURIComponent($.trim($('#displayKey').html()))
+}
+
+function copyKey() {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($.trim($('#displayKey').text())).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+function downloadMessage(){
+  window.location.href='data:application/txt,'+ encodeURIComponent($.trim($('#encryptedMessage').html()))
+}
+
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
