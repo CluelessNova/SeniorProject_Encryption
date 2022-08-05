cbc57c56e00f0ca4808ecfa2c3bed52389bde29c
CluelessNova <57334413+CluelessNova@users.noreply.github.com>
2022-08-04 21:53:02 -0400

    Adding Mobile Responsiveness
    First test version

--- Django/static/js/script1.js
+++ Django/static/js/script1.js
@@ -45,3 +45,12 @@ function copyKey() {
 function downloadMessage(){
   window.location.href='data:application/txt,'+ encodeURIComponent($.trim($('#encryptedMessage').html()))
 }
+
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
